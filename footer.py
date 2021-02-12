e25b32d34ca76a379d6283d56b7cba1dee31c7cd
jcamiloangarita <camiloang94@gmail.com>
2021-02-12 17:38:44 +0100

    add cover

--- footer.py
+++ footer.py
@@ -4,5 +4,7 @@ import streamlit as st
 def add_footer():
     # Final section
     st.write('_________________')
+    st.image('https://user-images.githubusercontent.com/52009346/69100304-2eb3e800-0a5d-11ea-9a3a-8e502af2120b.png',
+             use_column_width=True)
     st.subheader('About us')
     st.video('https://prowellplan.com/assets/images/Pro-Well-Plan-Produktvideo.mp4')
